8ab0376176bbf6b17fa9b458350c3230376f4f68
Bruno Peres <thelastdarkmage@gmail.com>
2022-12-02 21:39:46 -0300

    Added additional functionality
    - Renamed Program.cs to quicksharper.cs.
    - Now it remembers the last folder chosen. There's a config file named quicksharper.conf.
    - Handled some exceptions that could result from empty project names.

--- quicksharper.cs
+++ quicksharper.cs
@@ -3,6 +3,15 @@ using System.Diagnostics;
 
 class Program {
 	public static void Main(string[] args) {
+		// Remembers which folder the binary is in
+		string binPath = Directory.GetCurrentDirectory();
+		
+		// Get default project folder path in quicksharper.conf
+		string confPath = binPath + @"\quicksharper.conf"; // The config file location
+		string lastPath = File.Exists(confPath) ?
+			File.ReadAllText(confPath) // If the file exists, get the default path.
+			: @"Documents\GitHub"; // If there isn't any, default to GitHub's folder.
+		
 		// Goes to the Users directory
 		string userDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
 		Directory.SetCurrentDirectory(userDirectory);
@@ -14,12 +23,12 @@ class Program {
 		Console.WriteLine("-------------------------------------------\n");		
 		
 		// Asks the user where the project file should go, defaulting to Documents\GitHub.
-		Console.WriteLine(@"Where you want to create the project? (ENTER: Documents\GitHub)");
+		Console.WriteLine($"Where you want to create the project? (ENTER: {lastPath})");
 		string filePath = Console.ReadLine() ?? "";
 		
 		// If the user leaves the answer in blank, load the default values.
-		if (String.IsNullOrWhiteSpace(filePath)) {
-			filePath = @"Documents\GitHub";
+		if (String.IsNullOrWhiteSpace(filePath)) {			
+			filePath = lastPath;
 		}
 		
 		// Checks if the Path is valid, if not, create folders.
@@ -46,24 +55,40 @@ class Program {
 		string appType = Console.ReadLine() ?? "";
 		
 		// If the user leaves the answer in blank, load the default values.
-		if (String.IsNullOrWhiteSpace(appType)) {
+		if (String.IsNullOrWhiteSpace(appType)) {			
 			appType = "console";
 		}
 		
-		// Asks for the projects name
-		Console.Write("Project name: ");
-		string projectName = Console.ReadLine() ?? "";
+		string projectName = "";
+		
+		while (String.IsNullOrWhiteSpace(projectName)) {
+			// Asks for the projects name
+			Console.WriteLine("What should this project's name be? (ENTER: Project)");
+			string input = Console.ReadLine() ?? "";
+			
+			// If the user leaves the answer in blank, load the default values.
+			if (String.IsNullOrWhiteSpace(projectName)) {
+				projectName = "Project";
+			}
+			
+			// Checks if there's a project with the same name
+			if(Directory.Exists(projectName)) {
+				// Notifies the user about it
+				Console.WriteLine($"There is already a project with the name \"{projectName}\".");
+				
+				// Erases the variable's value, keeping the loop going
+				projectName = "";
+			}
+		}		
 		
 		// Create and enter the project's directory
 		Directory.CreateDirectory(projectName);
 		Directory.SetCurrentDirectory(projectName);
 		
 		// Invoke 'dotnet new' with the specified application template
-		Console.WriteLine(
-			$"Running \"dotnet new {appType}... " +
-			"A new terminal window should popup. ");
+		Console.WriteLine($"Running \"dotnet new {appType}... ");
 		Process dotnetProcess = Process.Start("CMD.exe",$"/C dotnet new {appType}");
-		dotnetProcess.WaitForExit(1000 * 60 * 2); // Wait up to two minutes
+		dotnetProcess.WaitForExit(1000 * 60 * 2); // Wait up to two minutes for timeout
 		
 		// Rename the Program.cs file to projectName.cs
 		System.IO.File.Move("Program.cs", projectName + ".cs");
@@ -73,5 +98,11 @@ class Program {
 		editorProcess.StartInfo.FileName = projectName + ".cs";
 		editorProcess.StartInfo.UseShellExecute = true;
 		editorProcess.Start();
+		
+		// Celebrate!
+		Console.WriteLine($"Project \"{projectName}\"succesfully created!");
+		
+		// Register the path used for this project, becoming the default for any future projects.
+		File.WriteAllText(binPath + @"\quicksharper.conf", filePath);
 	}
 }
